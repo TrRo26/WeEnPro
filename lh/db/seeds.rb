5083acfd7b66a782bf88524f3ada13241948bf58
Travis Roy <email": null>
2016-12-10 19:41:52 -0600

    Add seed data and additional setup

--- lh/db/seeds.rb
+++ lh/db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# HATE
+Item.create(title: 'Painting Over Lightswitches',
+            description: 'When someone is too lazy to take the 3 seconds to remove the light covers when painting a room.',
+            justification: 'cause it looks like shit when people do this!',
+            rating: '5',
+            category: 'hate' )
+
+Item.create(title: 'Rude Wrong Numbers',
+            description: 'When you get a call from a random number and the person is a dick.',
+            justification: 'they fucked up, they can at least not be an asshole',
+            rating: '3',
+            category: 'hate' )
+
+# LOVE
+Item.create(title: 'Sizzling Bacon',
+            description: 'The sound and smell of bacon sizzling in a hot pan.',
+            justification: 'bacon is incredible and tastes like pure bliss',
+            rating: '9',
+            category: 'love' )
+
+Item.create(title: 'Scotch',
+            description: 'Whisky from Scotland',
+            justification: "it's delicious and it gets you drunk",
+            rating: '10',
+            category: 'love' )
